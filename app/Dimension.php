27ab5c2999daf05aa745342f310ae1e77b1cef6d
João Salomão <joaosalomao.613@gmail.com>
2021-03-19 02:25:53 -0300

    Configuring models relations

--- app/Dimension.php
+++ app/Dimension.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dimension extends Model
 {
-    //
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
 }
--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function dimension()
+    {
+        return $this->belongsTo(Dimension::class);
+    }
 }
