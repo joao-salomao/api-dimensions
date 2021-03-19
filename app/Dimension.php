d49204f14f608416559cfad5d8b261fb48a42eb7
João Salomão <joaosalomao.613@gmail.com>
2021-03-19 03:37:50 -0300

    Defining fillable fields

--- app/Dimension.php
+++ app/Dimension.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dimension extends Model
 {
+    protected $fillable = [
+        'title'
+    ];
+
     public function questions()
     {
         return $this->hasMany(Question::class);
