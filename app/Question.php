17ed09a8ee0c94397419be4f0d208388dc073ba3
João Salomão <joaosalomao.613@gmail.com>
2021-03-19 03:37:04 -0300

    Adding casts and defining fillable fields

--- app/Question.php
+++ app/Question.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $casts = [
+        'active' => 'boolean'
+    ];
+
+    protected $fillable = [
+        'active',
+        'content',
+        'dimension_id',
+    ];
+
     public function dimension()
     {
         return $this->belongsTo(Dimension::class);
