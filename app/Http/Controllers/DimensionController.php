9efe79cee18b58dd79d00870a882666c5bfd3ce0
João Salomão <joaosalomao.613@gmail.com>
2021-03-19 02:00:21 -0300

    Creating dimension model

---
+++ app/Http/Controllers/DimensionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Dimension;
+use Illuminate\Http\Request;
+
+class DimensionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Dimension  $dimension
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dimension $dimension)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Dimension  $dimension
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Dimension $dimension)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Dimension  $dimension
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Dimension $dimension)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Dimension  $dimension
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dimension $dimension)
+    {
+        //
+    }
+}
---
+++ database/factories/DimensionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Dimension;
+use Faker\Generator as Faker;
+
+$factory->define(Dimension::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
