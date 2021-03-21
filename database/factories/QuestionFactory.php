248c63826ec55c8fb92981e7a828dfe3aaa96af9
João Salomão <joaosalomao.613@gmail.com>
2021-03-21 01:43:21 -0300

    Defining Question factory

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -3,10 +3,12 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Question;
+use App\Dimension;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        //
+        'content' => $faker->text,
+        'dimension_id' => (string) factory(Dimension::class)->create()->id
     ];
 });
